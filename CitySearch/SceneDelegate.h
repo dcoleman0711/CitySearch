c442c6b8aecea9f8fa3094506e604cc568e7e42c
Daniel Coleman <dcoleman0711@gmail.com>
2020-05-14 19:01:55 -0500

    Initial Commit

---
+++ CitySearch/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CitySearch
+//
+//  Created by Daniel Coleman on 5/14/20.
+//  Copyright © 2020 Daniel Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CitySearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CitySearch
+//
+//  Created by Daniel Coleman on 5/14/20.
+//  Copyright © 2020 Daniel Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
